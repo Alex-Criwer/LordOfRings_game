937ebf370adc5f672569a87289066979941e641b
Alex-Criwer <golub.av@phystech.edu>
2020-03-16 01:07:52 +0300

    test

---
+++ task2/A/preparing.py
@@ -0,0 +1,2 @@
+v = open("index.h", "w")
+v.write('int func(); //just function')
---
+++ task2/C/main.cpp
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <iostream>
+
+int myFunction(){
+ std::cout << "Hello , user " << "\n";
+ return 0;
+}
